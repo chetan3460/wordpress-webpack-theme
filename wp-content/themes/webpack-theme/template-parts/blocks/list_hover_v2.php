ca0b1de8cbde95cc12375954949a40dd4a233f4c
chetan dhargalkar <chetan@screenroot.com>
2024-08-12 14:22:39 +0530

    list hover v2 block completed

--- wp-content/themes/webpack-theme/template-parts/blocks/list_hover_v2.php
+++ wp-content/themes/webpack-theme/template-parts/blocks/list_hover_v2.php
@@ -18,25 +18,36 @@ if ($items_v2 && !$hide_block) :
 
 
 
-<?php if (!wp_is_mobile()) { ?>
+
 
 <section
     style="background-image: url('<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/homepage6-5.webp'); ?>');"
-    class="list_hover_v2 desktop py-[6.25rem] px-16 max-xl:px-5 max-md:py-12 max-container fade-in  w-full  bg-[center_center] bg-no-repeat bg-cover bg-fixed"
+    class="list_hover_v2  py-[6.25rem] px-16 max-xl:px-5 max-md:py-12 max-container fade-in  w-full  bg-[center_center] bg-no-repeat bg-cover bg-fixed"
     data-delay="0.2" style
     style="<?php if ($padding_top) { ?>padding-top: <?= $padding_top ?>px <?php } ?><?php if ($padding_bottom) { ?>padding-bottom: <?= $padding_bottom ?>px <?php } ?>">
 
-    <div class=" w-full   px-16 max-xl:px-5 pb-36">
-        <?php if ($sub_title) : ?>
-        <p class="text-xl leading-5 uppercase  max-md:mb-[1rem] mb-5 max-md:text-[1rem] text-[#7B7B7B]">
-            <?= $sub_title; ?>
-        </p>
-        <?php endif; ?>
+    <div class=" w-full max-xl:px-5 pb-16 flex md:flex-row flex-col justify-between">
+        <div class=" ">
+            <?php if ($sub_title) : ?>
+            <p class="text-xl leading-5 uppercase  max-md:mb-[1rem] mb-5 max-md:text-[1rem] text-[#7B7B7B]">
+                <?= $sub_title; ?>
+            </p>
+            <?php endif; ?>
 
-        <?php if ($heading) : ?>
-        <h2
-            class="text-6xl leading-[4rem] max-md:text-[2.5rem] max-md:leading-[3rem]  font-medium capitalize text-white font-medium">
-            <?= $heading; ?></h2>
+            <?php if ($heading) : ?>
+            <h2
+                class="text-6xl leading-[4rem] max-md:text-[2.5rem] max-md:leading-[3rem]  font-medium capitalize text-white font-medium">
+                <?= $heading; ?></h2>
+            <?php endif; ?>
+        </div>
+        <?php  if ($cta) : ?>
+        <div class="md:mt-0 mt-5">
+
+            <a href="<?= $cta['url']; ?>" target="<?= $cta_target; ?>" class="primary-btn  white"> <span
+                    class="btn-text"><span><?= $cta['title']; ?></span></span> <span class="btn-arrow"> <span
+                        class="icon-arrow-right"></span> </span> </a>
+
+        </div>
         <?php endif; ?>
     </div>
 
@@ -50,12 +61,16 @@ if ($items_v2 && !$hide_block) :
                         $title = $item['title'];
                         $desc = $item['description'];
                         $thumbnail = $item['image'];
+                        $item_link = $item['item_link']; // Get the item link from ACF
+
                         if (!$thumbnail) {
                             $thumbnail = get_stylesheet_directory_uri() . '/assets/images/placeholder.jpg';
                         }
                     ?>
         <div
-            class="showcase__item group  transition-all duration-700   flex min-h-[174px] justify-start items-center relative w-full pointer-events-auto pt-[11px] pb-1.5 px-[3.125%] border-solid border-t border-[#FFFFFF33] group">
+            class="showcase__item relative flex  lg:flex-row max-lg:flex-col justify-start items-center  py-[0.9em]  px-[3.125%] border-t border-white/20  lg:min-h-[11em] max-sl:min-h-[10em] w-full pointer-events-auto m-0 transition-all duration-700 group ">
+
+
 
 
 
@@ -63,33 +78,40 @@ if ($items_v2 && !$hide_block) :
 
 
             <?php if ($title || $desc) : ?>
+
+            <?php if ($item_link): ?>
+            <a href="<?= esc_url($item_link); ?>" class="absolute inset-0 z-20"></a> <!-- Full cover link -->
+            <?php endif; ?>
 
             <?php if ($title) : ?>
-            <h3 class="showcase__title">
-                <span class="showcase__subtitle">
+            <h3
+                class="showcase__title || relative   md:flex-row flex-col flex-grow  inline-flex md:items-center items-start md:text-4xl text-[1.7em] leading-[1.25em] tracking-tight font-medium  whitespace-normal lg:pr-[13rem] pr-0 lg:w-1/2 max-lg:w-full  xxl:pb-0 max-lg:pb-4  transform translate-x-0 lg:group-hover:translate-x-[6.5em] group-hover:translate-x-0 lg:group-[&.active]:translate-x-[6.5em] transition-all duration-[0.8s] z-10 ">
+                <span
+                    class="showcase__subtitle text-[14px] leading-[1.5em] inline-block relative w-[44px] min-w-fit text-center self-start mr-[24px] px-[6px] py-[3px] border border-[#7b7b7b] text-[#7b7b7b] rounded-[30px] transition-all duration-[0.4s] transform translate-y-[0.3em] flex-shrink-0">
                     0<?= $key + 1; ?></span>
-                <span class="title"> <?= $title; ?></span>
+                <span class=" title text-white transform skew-0 transition-all duration-700 md:mt-0 mt-4">
+                    <?= $title; ?></span>
             </h3>
             <?php endif; ?>
 
             <?php if ($desc) : ?>
-            <div class="showcase__content overflow-hidden w-[35%] text-white">
-                <p class=" text-base leading-7 max-md:max-w-full    transition-all duration-700 font-medium">
+            <div
+                class="showcase__content overflow-hidden w-[35%] max-lg:w-full text-white max-lg:mb-0 transition-all duration-[0.4s] transform translate-x-0">
+                <p
+                    class=" text-base leading-7 max-md:max-w-full    transition-all duration-700 font-medium xxl:pl-0 md:pl-[4.1em] pl-0">
                     <?= $desc; ?></p>
             </div>
             <?php endif; ?>
 
 
             <div
-                class="showcase__image interactive absolute h-auto left-[var(--wgl-image-position)] z-[4] opacity-0 -translate-y-2/4 origin-[top_center] pointer-events-none overflow-hidden top-2/4">
-
-                <img data-src="<?= $thumbnail['url'] ?>" alt="block image" class=" lazy-image " />
-
-
-
+                class="showcase__image interactive absolute h-auto   origin-[top_center] top-2/4 max-lg:hidden  left-0 transform -translate-y-1/2 z-[4] opacity-0 group-hover:opacity-100 pointer-events-none overflow-hidden transition-all duration-[0.45s] ml-[20px]">
+                <img data-src="<?= $thumbnail['url'] ?>" alt="block image"
+                    class=" lazy-image transform -translate-x-full group-hover:translate-x-0 transition-all duration-[0.8s] h-[140px]" />
             </div>
 
             <?php endif; ?>
+
 
             <?php endif; ?>
         </div>
@@ -97,64 +119,12 @@ if ($items_v2 && !$hide_block) :
     </div>
 
 
-    <?php  if ($cta) : ?>
-    <div class="flex justify-center w-full mt-20 max-md:mt-10">
 
-        <a href="<?= $cta['url']; ?>" target="<?= $cta_target; ?>" class="primary-btn bordered"> <span
-                class="btn-text"><span><?= $cta['title']; ?></span></span> <span class="btn-arrow"> <span
-                    class="icon-arrow-right"></span> </span> </a>
-
-    </div>
-    <?php endif; ?>
 
 
 </section>
-<?php } else { ?>
 
 
-<section class="img-content-block mob-tablet py-[6.25rem] max-md:py-12 px-16 max-xl:px-5  fade-in  flex-col gap-20 ">
-    <?php
-            foreach ($items_v2 as $key => $item) {
-                $thumbnail = $item['image'];
-                $title = $item['title'];
-                $desc = $item['description'];
-                if (!$thumbnail) {
-                    $thumbnail = get_stylesheet_directory_uri() . '/assets/images/placeholder.jpg';
-                }
-                // var_dump($term);
-
-            ?>
-
-    <a class="item-block flex gap-10 max-md:gap-5 max-md:flex-col cursor-default" href="javascript:void(0);">
-        <div class="flex col md:min-w-[50%]">
-            <img data-src="<?= $thumbnail['url']; ?>" class="object-cover  aspect-[0.875]   lazy-image" />
-        </div>
-        <div class="flex col flex-col">
-            <p class="mt-8 max-sl:mt-2 text-xl font-medium leading-6 text-black">0<?= $key + 1; ?></p>
-            <?php if ($title) : ?>
-            <h2 class="mt-5 max-md-3 text-4xl max-md:text-3xl font-medium leading-10 text-black capitalize">
-                <?= $title; ?>
-            </h2>
-            <?php endif; ?>
-            <?php if ($desc) : ?>
-            <p class="mt-8 max-md:mt-4 text-lg max-md:text-base leading-7 text-black sl:line-clamp-5">
-                <?= $desc; ?>
-            </p>
-            <?php endif; ?>
-            <!-- <div class="primary-btn mt-10 max-md:mt-5">
-                            <span class="btn-text"><span>learn more</span></span>
-                            <span class="btn-arrow">
-                                <span class="icon-arrow-right"></span>
-                            </span>
-                        </div> -->
-        </div>
-    </a>
-    <?php } ?>
-</section>
-
-<?php }
-
-    ?>
 
 
 
